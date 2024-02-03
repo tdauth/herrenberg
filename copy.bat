752e37b141341ce20c33298e26ebd8859c51bed2
Tamino Dauth <taminojdauth@gmail.com>
2024-02-03 18:15:10 +0100

    Zip

--- copy.bat
+++ copy.bat
@@ -3,4 +3,5 @@ xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\resources" "C:\Users\Tamino
 xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\resources" "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\mods\local\HerrenbergLocal" /s /e /Y
 xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\info.json" "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\mods\subscribed\211192_[COOP] Herrenberg" /s /e /Y
 xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\info.json" "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\mods\local\HerrenbergLocal" /s /e /Y
+zip -r -p "[COOP] Herrenberg.zip" "resources" "[COOP] Herrenberg.aoe2scenario"
 pause
