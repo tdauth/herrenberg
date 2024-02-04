233532027312b37aa7068a5f2eaa2eb7bcd590f1
Tamino Dauth <taminojdauth@gmail.com>
2024-02-04 21:19:36 +0100

    Fix folder structure

--- copy.bat
+++ copy.bat
@@ -1,7 +1,7 @@
-copy "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\resources\_common\scenario\[COOP] Herrenberg.aoe2scenario" "C:\Users\Tamino\Documents\Projekte\herrenberg"
+copy "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\resources\_common\scenario\[COOP] Herrenberg.aoe2scenario" "C:\Users\Tamino\Documents\Projekte\herrenberg\resources\_common\scenario"
 xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\resources" "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\mods\subscribed\211192_[COOP] Herrenberg\resources" /s /e /Y
 xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\resources" "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\mods\local\HerrenbergLocal\resources" /s /e /Y
 xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\info.json" "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\mods\subscribed\211192_[COOP] Herrenberg" /s /e /Y
 xcopy "C:\Users\Tamino\Documents\Projekte\herrenberg\info.json" "C:\Users\Tamino\Games\Age of Empires 2 DE\76561197992163765\mods\local\HerrenbergLocal" /s /e /Y
-zip -r -p "[COOP] Herrenberg.zip" "resources"
+zip -r -p "[COOP] Herrenberg.zip" "resources" "info.json" "thumbnail.jpg"
 pause
